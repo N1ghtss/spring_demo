4de82ba99b485e018cbdb4642b97dfa573ae812f
n1ghtss <635349188@qq.com>
2022-08-03 12:01:13 +0800

    8.3早

--- src/main/java/cn/night/controller/IndexController.java
+++ src/main/java/cn/night/controller/IndexController.java
@@ -5,8 +5,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-    @GetMapping("/index")
+
+    // 跳转系统主页
+    @GetMapping("index")
     public String login() {
         return "index";
+    }
+
+    // 跳转用户基本信息页面
+    @GetMapping("info")
+    public String info() {
+        return "info";
+    }
+
+    @GetMapping("pwd")
+    public String pwd() {
+        return "pwd";
     }
 }
